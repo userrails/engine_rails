62e94c8fe2b52895dd01370135206972f7937e99
Shiv Raj Badu <shivrajbadu@gmail.com>
2015-11-16 15:27:22 +0545

    tested gem bundler inside engines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116044501) do
+ActiveRecord::Schema.define(version: 20151116090325) do
+
+  create_table "blorgh_articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "foo_bar_kitties", force: :cascade do |t|
     t.string   "name"
--- lib/engines/blorgh/blorgh.gemspec
+++ lib/engines/blorgh/blorgh.gemspec
@@ -20,4 +20,7 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 4.2.4"
 
   s.add_development_dependency "sqlite3"
+
+  s.add_dependency "devise"
+  s.add_dependency 'authority', '~> 3.1'
 end
