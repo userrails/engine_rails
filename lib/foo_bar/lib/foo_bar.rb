12d5dc81d912da6e49f50581c96a0c7bbea14a5c
Shiv Raj Badu <shivrajbadu@gmail.com>
2015-11-16 12:32:34 +0545

    auto load app/serives, app/workers dirs

--- lib/foo_bar/lib/foo_bar.rb
+++ lib/foo_bar/lib/foo_bar.rb
@@ -1,4 +1,11 @@
 require "foo_bar/engine"
 
 module FooBar
+	# def self.load_files
+	#     [
+	#       "app/workers/mailer_worker",
+	#       "app/services/kitty_washer"
+	#     ]
+ # 	 end
 end
+
--- lib/foo_bar/lib/foo_bar/engine.rb
+++ lib/foo_bar/lib/foo_bar/engine.rb
@@ -2,14 +2,23 @@ module FooBar
   class Engine < ::Rails::Engine
     isolate_namespace FooBar
 
+    #This is useful to load plugin automatically in rake routes, db:migrate, etc
     initializer "foo_bar", before: :load_config_initializers do |app|
       Rails.application.routes.append do
         mount FooBar::Engine, at: "/foo_bar"
       end
 
       config.paths["db/migrate"].expanded.each do |expanded_path|
-	    Rails.application.config.paths["db/migrate"] << expanded_path
-	  end
+         Rails.application.config.paths["db/migrate"] << expanded_path
+      end
     end
+
+    #This is useful when we want to load some other files like app/services, app/workers, etc automatically which is inside lib/foobar.rb
+    initializer "foo_bar", after: :load_config_initializers do |app|
+      FooBar.load_files.each { |file|
+        require_relative File.join("../..", file)
+      }
+    end
+    
   end
 end
